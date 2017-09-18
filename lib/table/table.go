0e19c2bd753ae95da9cf05ca3a2ba88268c41bf4
george <me@georgemac.com>
2017-09-18 12:02:16 +0100

    Support generation of multiple table tests

--- lib/table/table.go
+++ lib/table/table.go
@@ -14,23 +14,23 @@ import (
 // table definition for a package, type and func
 // name
 type Table struct {
-	Package  string
-	Type     string
-	Function string
+	Package   string
+	Type      string
+	Functions []string
 }
 
 // New configures a Table type definition
-func New(pkg, typ, fn string) Table {
+func New(pkg, typ string, funcs ...string) Table {
 	return Table{
-		Package:  pkg,
-		Type:     typ,
-		Function: fn,
+		Package:   pkg,
+		Type:      typ,
+		Functions: funcs,
 	}
 }
 
 // CaseType returns the name of the test case type
 // to be generated
-func (t Table) CaseType() string { return fmt.Sprintf("%s%sCase", strings.ToLower(t.Type), t.Function) }
+func (t Table) CaseType(fn string) string { return fmt.Sprintf("%s%sCase", strings.ToLower(t.Type), fn) }
 
 // WriteTestTo writes out the test definition to the writer
 func (t Table) WriteTestTo(w io.Writer) (int64, error) {
@@ -56,22 +56,26 @@ var (
 	testTmpl = template.Must(template.New("test").Parse(`package {{ .Package }}
 
 import "testing"
+{{ range .Functions }}
 
-func Test{{ .Type }}{{ .Function }}(t *testing.T) {
-	for _, testCase := range []{{ .CaseType }}{
+func Test{{ $.Type }}{{ . }}(t *testing.T) {
+	for _, testCase := range []{{ $.CaseType . }}{
 		{name: "happy path"},
 	} {
 		t.Run(testCase.name, testCase.Run)
 	}
-}`))
+}
+{{ end }}`))
 
 	defTmpl = template.Must(template.New("tableDef").Parse(`package {{ .Package }} 
 
 import "testing"
+{{ range .Functions }}
 
-type {{ .CaseType }} struct {
+type {{ $.CaseType . }} struct {
 	name string
 }
 
-func (c {{ .CaseType }}) Run(t *testing.T) {}`))
+func (c {{ $.CaseType . }}) Run(t *testing.T) {}
+{{ end }}`))
 )
